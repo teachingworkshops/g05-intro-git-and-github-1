ecab465ec1b3d3d558e9424e8f16e99aab9a2062
Noah Buteau <buteaun@wit.edu>
2024-01-30 20:08:06 -0500

    Made it so you can move to all maps in the game and also made it so you can see the map as well

--- movement.py
+++ movement.py
@@ -1,13 +1,32 @@
 import os
 import msvcrt
+from maps.map_structure import Map
+from maps.places import find_backround
 
-def play_game(background, position):
+def play_game(position, entry_point) -> tuple[(int, int), bool, str]:
 
+    #taking the position of the player and finding the background for each one 
+    background = find_backround.find_backround(position[0], position[1])
 
     screen_width = len(background[0])
     screen_height = len(background)
-    player_x = screen_width // 2
-    player_y = screen_height // 2
+    # To change players starting postion change the player_x and player_y
+    #a bunch of if statments is silly but it works for now
+    if entry_point == "left":
+        player_x = 1
+        player_y = screen_height // 2
+    elif entry_point == "right":
+        player_x = screen_width - 2
+        player_y = screen_height // 2
+    elif entry_point == "top":
+        player_x = screen_width // 2
+        player_y = 1
+    elif entry_point == "bottom":
+        player_x = screen_width // 2
+        player_y = screen_height - 2
+    else:
+        player_x = 1
+        player_y = screen_height // 2
 
     def getch():
         return msvcrt.getch()
@@ -27,9 +46,11 @@ def play_game(background, position):
         background[player_y] = "".join(updated_row)
 
     def is_valid_move(x, y):
-        if x < 0 or x >= screen_width or y < 0 or y >= screen_height:
+        # Check if x and y are within the valid range
+        if 0 <= y < len(background) and 0 <= x < len(background[y]):
+            return background[y][x] == " "
+        else:
             return False
-        return background[y][x] == " "
 
     def move_player(x, y):
         if is_valid_move(x, y):
@@ -38,15 +59,14 @@ def play_game(background, position):
             return x, y
         return player_x, player_y
 
-    print("Use WASD keys to move, 'q' to quit")
 
     while True:
         draw_game()
         ch = getch()
 
-        if ch == b'q':
-            print("Thanks for playing!")
-            break
+        # if ch == b'q':
+        #     print("Thanks for playing!")
+        #     print(position)
 
         if ch == b'a':
             player_x, player_y = move_player(player_x - 1, player_y)
@@ -58,29 +78,29 @@ def play_game(background, position):
             player_x, player_y = move_player(player_x, player_y + 1)
 
         if player_x == 0:
-            print("You hit the left of the screen")
             x = position[0] - 1
             position = (x,position[1])
-            print(f"Your position {position} decreased by one in the x direction ")
+            entry_point = "right"
             break
         elif player_x == screen_width - 1:
-            print("You hit the right of the screen")
-            x =position[0] + 1 
+            x = position[0] + 1 
             position = (x,position[1])
-            print(f"Your position {position} increased by one in the x direction ")
+            entry_point = "left"
             break
         elif player_y == 0:
-            print("You hit the top of the screen")
             y = position[1] - 1
             position = (position[0],y)
-            print(f"Your position {position} decreased by one in the y direction ")
+            entry_point = "bottom"
             break
         elif player_y == screen_height - 1:
-            print("You hit the bottom of the screen!")
             y = position[1] + 1
             position = (position[0],y)
-            print(f"Your position {position} increased by one in the y direction ")
+            entry_point = "top"
             break
+    #will change this to return if they won or not 
+    if position == (2,4):
+        return position, True, entry_point
+    return position, False, entry_point
     
     
 background = [
@@ -101,4 +121,4 @@ background = [
     ]
 
 
-play_game(background, (2,2))
+# play_game(Map.ruggles_background, (2,2))
